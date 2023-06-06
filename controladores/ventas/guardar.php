0dfa5c9cac2fc8d4e0797ca1a682af3b17b674a4
¨Howard <programadorhaaj.2023@gmail.com>
2023-06-05 23:27:50 -0600

    Roo

--- controladores/ventas/guardar.php
+++ controladores/ventas/guardar.php
@@ -4,11 +4,8 @@ require_once '../../modelos/Detalle.php';
 
 $productos = array_filter($_POST['productos']);
 $cantidades = array_filter($_POST['cantidades']);
-$_POST['venta_fecha'] = str_replace('T',' ', $_POST['venta_fecha']);
-if($_POST['venta_cliente'] != '' && $_POST['venta_fecha'] != '' && count($productos)>0 && count($cantidades)>0){
-
-    
-
+$_POST['venta_fecha'] = str_replace('T', ' ', $_POST['venta_fecha']);
+if ($_POST['venta_cliente'] != '' && $_POST['venta_fecha'] != '' && count($productos) > 0 && count($cantidades) > 0) {
 
     try {
         $venta = new Venta($_POST);
@@ -23,29 +20,20 @@ if($_POST['venta_cliente'] != '' && $_POST['venta_fecha'] != '' && count($produc
             ]);
             $detalle->guardar();
             $i++;
-
         }
-
-        
     } catch (PDOException $e) {
         $error = $e->getMessage();
-    } catch (Exception $e2){
+    } catch (Exception $e2) {
         $error = $e2->getMessage();
     }
-}else{
+} else {
     $error = "Debe llenar todos los datos y seleccionar al menos un producto";
 }
-
-
-// if($resultado){
-//     echo "Guardado exitosamente";
-// }else{
-//     echo "Ocurrió un error: $error";
-// }
 
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -53,27 +41,29 @@ if($_POST['venta_cliente'] != '' && $_POST['venta_fecha'] != '' && count($produc
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Resultados</title>
 </head>
+
 <body>
     <div class="container">
         <div class="row">
             <div class="col-lg-6">
-                <?php if($resultado): ?>
+                <?php if ($resultado) : ?>
                     <div class="alert alert-success" role="alert">
                         Guardado exitosamente!
                     </div>
-                <?php else :?>
+                <?php else : ?>
                     <div class="alert alert-danger" role="alert">
                         Ocurrió un error: <?= $error ?>
                     </div>
                 <?php endif ?>
-              
+
             </div>
         </div>
         <div class="row">
             <div class="col-lg-4">
-                <a href="/crud_practica9/vistas/ventas/index.php" class="btn btn-info">Volver al formulario</a>
+                <a href="/Practica-No-9/vistas/ventas/index.php" class="btn btn-info">Volver al formulario</a>
             </div>
         </div>
     </div>
 </body>
+
 </html>
