a5a18364ad20949570b270f622d904d4db86ac80
¨Howard <programadorhaaj.2023@gmail.com>
2023-06-05 22:41:18 -0600

    error de formato

--- vistas/ventas/index.php
+++ vistas/ventas/index.php
@@ -6,8 +6,7 @@ try {
     $producto = new Producto();
     $clientes = $cliente->buscar();
     $productos = $producto->buscar();
-    // var_dump($clientes);
-    // exit;
+
 } catch (PDOException $e) {
     $error = $e->getMessage();
 } catch (Exception $e2) {
@@ -17,9 +16,9 @@ try {
 <?php include_once '../../includes/header.php' ?>
 <?php include_once '../../includes/navbar.php' ?>
 <div class="container">
-    <h1 class="text-center">Formulario de ingreso de ventas</h1>
+    <h1 class="text-center">Formulario de ingreso de las ventas</h1>
     <div class="row justify-content-center">
-        <form action="/crud_practica9/controladores/ventas/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+        <form action="/Practica-No-9/controladores/ventas/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
             <div class="row mb-3">
                 <div class="col">
                     <label for="venta_cliente">Cliente</label>
